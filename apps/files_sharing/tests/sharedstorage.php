150baa4e1b6762dc44637244c23607015b7e8134
Bjoern Schiessle <schiessle@owncloud.com>
2014-04-30 16:56:09 +0200

    move the mount point up if a share is mounted to a subfolder and the subfolder no longer exists

---
+++ apps/files_sharing/tests/sharedstorage.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * ownCloud
+ *
+ * @author Bjoern Schiessle
+ * @copyright 2014 Bjoern Schiessle <schiessle@owncloud.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+require_once __DIR__ . '/base.php';
+
+use OCA\Files\Share;
+
+/**
+ * Class Test_Files_Sharing_Api
+ */
+class Test_Files_Sharing_Storage extends Test_Files_Sharing_Base {
+
+	function setUp() {
+		parent::setUp();
+
+		$this->folder = '/folder_share_storage_test';
+
+		$this->filename = '/share-api-storage.txt';
+
+		// save file with content
+		$this->view->mkdir($this->folder);
+	}
+
+	function tearDown() {
+		$this->view->deleteAll($this->folder);
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @medium
+	 */
+	function testDeleteParentOfMountPoint() {
+
+		// share to user
+		$fileinfo = $this->view->getFileInfo($this->folder);
+		$result = \OCP\Share::shareItem('folder', $fileinfo['fileid'], \OCP\Share::SHARE_TYPE_USER,
+			self::TEST_FILES_SHARING_API_USER2, 31);
+
+		$this->assertTrue($result);
+
+		self::loginHelper(self::TEST_FILES_SHARING_API_USER2);
+		$user2View = new \OC\Files\View('/' . self::TEST_FILES_SHARING_API_USER2 . '/files');
+		$this->assertTrue($user2View->file_exists($this->folder));
+
+		// create a local folder
+		$result = $user2View->mkdir('localfolder');
+		$this->assertTrue($result);
+
+		// move mount point to local folder
+		$result = $user2View->rename($this->folder, '/localfolder/' . $this->folder);
+		$this->assertTrue($result);
+
+		// mount point in the root folder should no longer exist
+		$this->assertFalse($user2View->is_dir($this->folder));
+
+		// delete the local folder
+		$result = $user2View->unlink('/localfolder');
+		$this->assertTrue($result);
+
+		//enforce reload of the mount points
+		self::loginHelper(self::TEST_FILES_SHARING_API_USER2);
+
+		//mount point should be back at the root
+		$this->assertTrue($user2View->is_dir($this->folder));
+	}
+
+}
